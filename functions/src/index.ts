ba5f524e924806e9758fa3eadb6313ed8f0a7c25
mooner92 <chlaudgjs0989@gmail.com>
2024-08-07 21:31:36 +0900

    add function files

--- functions/src/index.ts
+++
@@ -1,9 +0,0 @@
-//functions의 엔트리포인트로 사용할거야
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,8 +1,22 @@
+import * as functions from 'firebase-functions';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Express } from 'express'; // import express as default import
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const server: Express = express();
+
+const createNestServer = async (expressInstance: Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  await app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('NestJS server created'))
+  .catch((err) => console.error('NestJS server creation error', err));
+
+export const api = functions.https.onRequest(server);
