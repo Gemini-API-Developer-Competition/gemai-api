19fe8a06625a034393f3a2fe0d0c5f24232a5c12
mooner92 <chlaudgjs0989@gmail.com>
2024-08-07 23:51:29 +0900

    mod ver0.0.1

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,12 +1,34 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import express, { Express } from 'express';
+
+// const server: Express = express();
+
+// const createNestServer = async (expressInstance: Express) => {
+//   const app = await NestFactory.create(
+//     AppModule,
+//     new ExpressAdapter(expressInstance),
+//   );
+//   app.enableCors();
+//   await app.init();
+// };
+
+// createNestServer(server)
+//   .then(() => console.log('NestJS server created'))
+//   .catch((err) => console.error('NestJS server creation error', err));
+
+// export const api = functions.https.onRequest(server);
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express, { Express } from 'express';
+import express from 'express';
+import { onRequest } from 'firebase-functions/v2/https';
 
-const server: Express = express();
+const server = express();
 
-const createNestServer = async (expressInstance: Express) => {
+const createNestServer = async (expressInstance: express.Express) => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
@@ -19,4 +41,4 @@ createNestServer(server)
   .then(() => console.log('NestJS server created'))
   .catch((err) => console.error('NestJS server creation error', err));
 
-export const api = functions.https.onRequest(server);
+export const api = onRequest({ region: ['asia-northeast3'] }, server);
