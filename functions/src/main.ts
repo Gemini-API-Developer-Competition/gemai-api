825a4fe9e8cebc00fbd47d675edcdcce6083c275
mooner92 <chlaudgjs0989@gmail.com>
2024-08-12 17:53:40 +0900

    mod# add CORS settings

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,25 +1,3 @@
-// import * as functions from 'firebase-functions';
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import express, { Express } from 'express';
-
-// const server: Express = express();
-
-// const createNestServer = async (expressInstance: Express) => {
-//   const app = await NestFactory.create(
-//     AppModule,
-//     new ExpressAdapter(expressInstance),
-//   );
-//   app.enableCors();
-//   await app.init();
-// };
-
-// createNestServer(server)
-//   .then(() => console.log('NestJS server created'))
-//   .catch((err) => console.error('NestJS server creation error', err));
-
-// export const api = functions.https.onRequest(server);
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
@@ -33,7 +11,9 @@ const createNestServer = async (expressInstance: express.Express) => {
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   await app.init();
 };
 
